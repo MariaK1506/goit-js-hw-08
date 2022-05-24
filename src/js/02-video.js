5b23ceddd7294ef74d420951d1aa6a3926c7dd2d
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-05-24 16:56:13 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,28 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+const onPlay = function (event) {
+  //   console.log(event.seconds);
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+};
 
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+// const qwe = _.throttle(onPlay, 1000);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });

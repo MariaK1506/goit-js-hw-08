c08fcee07a3407f5ab2f7d799cd2ab0d962854d9
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-05-26 12:44:20 +0300

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,36 +8,30 @@ const refs = {
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.input.addEventListener('input', onInputChange);
-refs.textarea.addEventListener('input', onInputChange);
+refs.form.addEventListener('input', throttle(onFormDataChange, 500));
 
-let message;
-
-function onInputChange(event) {
-  //   event.preventDefault();
-  //   const email = event.target.value;
+function onFormDataChange(event) {
   const email = refs.input.value;
-  //   const message = event.target.value;
-  message = refs.textarea.value;
+  const message = refs.textarea.value;
   const formData = { email, message };
-
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-
-  //
-  //   console.log(refs.input.value);
-  //   console.log(refs.textarea.value);
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
-  //   localStorage.clear();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+
+  localStorage.removeItem('feedback-form-state');
   event.currentTarget.reset();
 }
 
+populateFormData();
+
 function populateFormData() {
-  const savedStorage = JSON.parse(localStorage.getItem('feedback-form-state'));
-  if (savedStorage) {
-    console.log(savedStorage);
-    message = savedStorage;
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (savedData) {
+    refs.input.value = savedData.email;
+    refs.textarea.value = savedData.message;
   }
 }

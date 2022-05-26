8bc0c50a868ab50bdb90ce527fec9872bd7b6328
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-05-26 14:29:30 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,6 +12,8 @@ const refs = {
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormDataChange, 500));
 
+populateFormData();
+
 function onFormDataChange() {
   const email = refs.input.value;
   const message = refs.textarea.value;
@@ -26,8 +28,6 @@ function onFormSubmit(event) {
   localStorage.removeItem(STORAGE_KEY);
   event.currentTarget.reset();
 }
-
-populateFormData();
 
 function populateFormData() {
   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));

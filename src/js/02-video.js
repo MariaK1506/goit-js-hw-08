50371ad34eeaef15eadefa18cdc81cca9d856f45
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-05-26 14:18:36 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
 const onPlay = function (event) {
   //   console.log(event.seconds);
-  localStorage.setItem('videoplayer-current-time', event.seconds);
+  localStorage.setItem(STORAGE_KEY, event.seconds);
 };
 
 // const qwe = _.throttle(onPlay, 1000);
@@ -14,7 +16,7 @@ const onPlay = function (event) {
 player.on('timeupdate', throttle(onPlay, 1000));
 
 player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
   .then(function (seconds) {
     // seconds = the actual time that the player seeked to
   })
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,7 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('input'),
@@ -10,25 +12,25 @@ const refs = {
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormDataChange, 500));
 
-function onFormDataChange(event) {
+function onFormDataChange() {
   const email = refs.input.value;
   const message = refs.textarea.value;
   const formData = { email, message };
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
 
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(STORAGE_KEY);
   event.currentTarget.reset();
 }
 
 populateFormData();
 
 function populateFormData() {
-  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
   if (savedData) {
     refs.input.value = savedData.email;
